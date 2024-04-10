e5fd591d6720fb09b29b4c252333748efc5632e0
pbabell <phillip.b.abell@gmail.com>
2024-04-10 11:41:08 -0400

    Nested references and example

--- src/convert_philter.py
+++ src/convert_philter.py
@@ -1,13 +1,11 @@
 import re
 import sys
 
+# Read in the file
 file_path = sys.argv[1]
 file_prefix = file_path.removesuffix(".philter")
 with open(file_path, 'r') as file:
     philter = file.read()
-
-if not philter.endswith("\n"):
-    philter = philter + "\n"
 
 # Helper class for regex'n
 class Regex:
@@ -26,7 +24,7 @@ class Regex:
             return None
 
 regex_entry = Regex("(?s)\$", "*? = (\{.*?\s\})(?=\s|$)")
-regex_to_replace = Regex("(?s)\%", "\s")
+regex_to_replace = Regex("(?s)\%(", ")(\s*?)$")
 
 # Get all key-values
 regex_entry.inject_value("([^\n ]*)")
@@ -36,20 +34,25 @@ filter_entries = {}
 # For each entry, find any %<var> to replace, then remove the entry
 for matchNum, match in enumerate(matches, start=1):
     filter_key = match.group(1)
-    # remove brackets, first newline, and first tab
-    filter_val = match.group(2).replace('{', '').replace('}', '').replace('\n', '', 1).replace('    ', '', 1)
+    # remove brackets, strip all leading and trailing whitespace
+    filter_val = match.group(2).strip('\{\}').strip()
+    # print(f"Found {filter_key} with value \n    {filter_val}")
+    regex_to_replace.inject_value(".*?")
+    nested_matches = re.finditer(regex_to_replace.get_regex(), filter_val, re.MULTILINE)
+
+    # Resolve nested references
+    filter_val = re.sub(regex_to_replace.get_regex(), lambda m : filter_entries[m.group(1).strip()], filter_val, flags=re.MULTILINE)
 
     filter_entries[filter_key] = filter_val
-    # print(f"Found {filter_key} with \n{filter_val}")
 
     regex_entry.inject_value(filter_key)
     philter = re.sub(regex_entry.get_regex(), '', philter)
     regex_to_replace.inject_value(filter_key)
-    philter = re.sub(regex_to_replace.get_regex(), filter_entries[filter_key], philter)
+    philter = re.sub(regex_to_replace.get_regex(), filter_entries[filter_key], philter, flags=re.MULTILINE)
 
 # cleanup and remove any leading newlines
 philter = philter.strip()
 
-# write out
+# write out to the .filter
 with open(file_prefix + ".filter", "w") as out:
     out.write(philter)
