29bf9a95141b62c670c153d88905049eba4059d3
pbabell <phillip.b.abell@gmail.com>
2024-05-09 16:46:52 -0400

    removed spaces from example

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 call venv\Scripts\activate
 echo Y | del dist
 pyinstaller --noconsole --noconfirm --onefile .\src\convert_philter.py
-echo f | xcopy dist\convert_philter.exe installer\convert_philter.exe /E
+echo Y | xcopy dist\convert_philter.exe installer\convert_philter.exe /E
