b9e81789b85d870cc8b56b94e3df1288b1467bc0
pbabell <phillip.b.abell@gmail.com>
2025-01-03 13:48:18 -0500

    build

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-call venv\Scripts\activate
+call ..\venv\Scripts\activate
 echo Y | del dist
 pyinstaller --noconsole --noconfirm --onefile .\src\convert_philter.py
 echo Y | xcopy dist\convert_philter.exe installer\convert_philter.exe /E
